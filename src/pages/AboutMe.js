3db9083552b8b3ef05b52592c5536e077f3f88b0
erling <nupen.erling@gmail.com>
2025-01-26 17:38:28 +0100

    fixed layout issue on phone

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -7,10 +7,14 @@ function AboutMe() {
       <h1>About Me</h1>
       <div className="about-summary">
         <p>
-          Hi, I'm Erling, a highly motivated and enthusiastic data scientist with a master's degree in Particle Physics from the University of Oslo. I have a strong foundation in machine learning, data analysis, image analysis, mathematics, and various programming languages. I am passionate about leveraging my skills to solve complex problems and make data-driven decisions.
+          Hi, I'm Erling, a highly motivated and enthusiastic data scientist with a master's degree in Particle Physics from the University of Oslo. 
+          I have a strong foundation in machine learning, data analysis, image analysis, mathematics, and various programming languages. I am passionate about using my skills to solve complex problems and make data-driven decisions.
         </p>
         <p>
-          With a keen interest in continuous learning, I seek opportunities in dynamic and innovative environments where I can use my knowledge and grow professionally. Over the past year, as my Python and coding skills have significantly improved, I have developed a passion for various fun DIY projects, and I can't wait to experiment using affordable hardware such as Raspberry Pi and a range of sensors to create cool and interesting projects. I am excited to share the results of the various projects, as well as other intersting things i might come across.
+          With a keen interest in continuous learning, I seek opportunities in dynamic and innovative environments where 
+          I can use my knowledge and grow professionally. Over the past year, as my Python and coding skills have significantly improved,
+          I have developed a passion for various fun DIY projects, and I can't wait to experiment using affordable hardware such as Raspberry Pi 
+          and a range of sensors to create cool and interesting projects. I am excited to share the results of the various projects, as well as other intersting things i might come across.
           This website will be my central hub of information where I can put out a range of information, not just the DIY projects I am currently working on.
         </p>
       </div>
@@ -22,7 +26,7 @@ function AboutMe() {
         <p>As I continue to develop my career, I aim to:</p>
         <ul>
           <li>Stay updated with the latest advancements in data science and machine learning.</li>
-          <li>Obtain certifications in Azure and other relevant technologies.</li>
+          <li>Get certifications in Azure and other relevant technologies.</li>
           <li>Contribute to impactful projects where I can utilize my skills in data analysis and machine learning to solve real-world problems.</li>
           <li>Collaborate with like-minded professionals and expand my professional network.</li>
         </ul>
